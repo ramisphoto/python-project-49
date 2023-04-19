a8175a551966afe35e5164993a745b59b8b21b0b
macbook <ramisphoto>
2023-04-19 23:21:13 +0300

    create a dir games

---
+++ brain_games/games/calc.py
@@ -0,0 +1,37 @@
+import prompt
+from random import randint
+from random import choice
+from brain_games.cli import welcome_user
+
+
+name = welcome_user()
+
+
+def calc():
+    print('What is the result of the expression?')
+    operators = ['+', '-', '*']
+    question_count = 0
+    while question_count != 3:
+        number_1 = randint(0, 100)
+        number_2 = randint(0, 100)
+        number_3 = randint(0, 11)
+        random_operators = choice(operators)
+        if random_operators == operators[0]:
+            math_question = f'{number_1} + {number_2}'
+            result = str(number_1 + number_2)
+        elif random_operators == operators[1]:
+            math_question = f'{number_1} - {number_2}'
+            result = str(number_1 - number_2)
+        elif random_operators == operators[2]:
+            math_question = f'{number_1} * {number_3}'
+            result = str(number_1 * number_3)
+        print(f'Question: {math_question}')
+        answer = prompt.string('Your answer: ')
+        if answer == result:
+            print('Correct!')
+            question_count = question_count + 1
+        else:
+            question_count = 0
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{result}'. Let's try again, {name}!")
+    print(f"Congratulation, {name}!")
---
+++ brain_games/games/even.py
@@ -0,0 +1,27 @@
+import prompt
+from random import randint
+from brain_games.cli import welcome_user
+
+name = welcome_user()
+
+
+def even():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    question_count = 0
+    while question_count != 3:
+        number = randint(0, 1000)
+        math_question = f'{number}'
+        if number % 2 == 0:
+            result = 'yes'
+        else:
+            result = 'no'
+        print(f'Question: {math_question}')
+        answer = prompt.string('Your answer: ')
+        if answer == result:
+            print('Correct!')
+            question_count = question_count + 1
+        else:
+            question_count = 0
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{result}'. Let's try again, {name}!")
+    print(f'Congratulations, {name}!')
