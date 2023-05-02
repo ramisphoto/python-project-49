698d5129f457c9c811aaf797989fb117da5f74b6
macbook <ramisphoto>
2023-05-02 12:38:38 +0300

    # list_number_1

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -14,7 +14,7 @@ def progression():
         number_1 = randint(1, 10)
         number_2 = randint(1, 100)
         for_fun = randint(4, 10)
-        list_numbers_1 = range(number_1, number_1 + 10)
+#        list_numbers_1 = range(number_1, number_1 + 10)
         list_numbers_2 = range(number_2, number_2 + (10 * for_fun), for_fun)
 #        sq = list(map(lambda num: str(num ** 2), list_numbers_1))
         sub = list(map(lambda num: str(num - number_1), reversed(list_numbers_2)))
@@ -27,7 +27,7 @@ def progression():
 #            div.append(next_one)
 #            previous = current
 #            current = next_one
- #       div = list(map(str, div))
+#       div = list(map(str, div))
         random_index = randint(0, 9)
         list_progression = [sub, summ]
         progression = choice(list_progression)
