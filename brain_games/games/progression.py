e3ef08dedeb285f3d435c68c568193a401067140
macbook <ramisphoto>
2023-05-02 11:36:27 +0300

    no div, sq

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -16,20 +16,20 @@ def progression():
         for_fun = randint(4, 10)
         list_numbers_1 = range(number_1, number_1 + 10)
         list_numbers_2 = range(number_2, number_2 + (10 * for_fun), for_fun)
-        sq = list(map(lambda num: str(num ** 2), list_numbers_1))
+#        sq = list(map(lambda num: str(num ** 2), list_numbers_1))
         sub = list(map(lambda num: str(num - number_1), reversed(list_numbers_2)))
         summ = list(map(lambda num: str(num + number_1), list_numbers_2))
-        div = [number_1]
-        previous = 0
-        current = number_1
-        for index in range(0, 9):
-            next_one = current + previous
-            div.append(next_one)
-            previous = current
-            current = next_one
-        div = list(map(str, div))
+#        div = [number_1]
+#        previous = 0
+#        current = number_1
+#        for index in range(0, 9):
+#            next_one = current + previous
+#            div.append(next_one)
+#            previous = current
+#            current = next_one
+ #       div = list(map(str, div))
         random_index = randint(0, 9)
-        list_progression = [sq, sub, summ, div]
+        list_progression = [sub, summ]
         progression = choice(list_progression)
         result = progression[random_index]
         progression.insert(random_index, '..')
