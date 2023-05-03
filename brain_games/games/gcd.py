dc2b337ac9ecd0284896d860d607960dd124db07
macbook <ramisphoto>
2023-05-03 10:40:06 +0300

    fix linter gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -6,6 +6,26 @@ from brain_games.cli import welcome_user
 name = welcome_user()
 
 
+def find_divider(number_1, number_2):
+    if number_1 >= number_2:
+        divider = number_2
+        rest = number_1 % divider
+    else:
+        divider = number_1
+        rest = number_2 % divider
+    if rest == 0:
+        result = divider
+    while divider and rest != 0:
+        divider = divider % rest
+        if divider == 0:
+            result = rest
+            break
+        rest = rest % divider
+        if rest == 0:
+            result = divider
+            break
+    return result 
+
 def gcd():
     print('Find the greatest common divisor of given numbers.')
     question_count = 0
@@ -13,23 +33,7 @@ def gcd():
         number_1 = randint(1, 100)
         number_2 = randint(1, 100)
         math_question = f'{number_1} {number_2}'
-        if number_1 >= number_2:
-            divider = number_2
-            rest = number_1 % divider
-        else:
-            divider = number_1
-            rest = number_2 % divider
-        if rest == 0:
-            result = divider
-        while divider and rest != 0:
-            divider = divider % rest
-            if divider == 0:
-                result = rest
-                break
-            rest = rest % divider
-            if rest == 0:
-                result = divider
-                break
+        result = find_divider(number_1, number_2)
         print(f'Question: {math_question}')
         answer = prompt.string('Your answer: ')
         if answer == str(result):
