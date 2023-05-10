4cf3a5b3586c017f58573044a0a9d9c18a8d1525
macbook <ramisphoto>
2023-05-10 10:38:30 +0300

    fix prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,7 +7,7 @@ game_rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def get_question_and_right_answer():
     number = randint(0, 100)
     div = 0
-    for i in range(2, number//2 + 1):
+    for i in range(2, number // 2 + 1):
         if number % i == 0:
             div = div + 1
             break
