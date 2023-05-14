3df213069935e7733b8c570db8a8d4dfba00be26
macbook <ramisphoto>
2023-05-14 23:45:53 +0300

    upgrade calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,26 +3,23 @@ from random import choice
 
 
 game_rule = 'What is the result of the expression?'
+operators = ['+', '-', '*']
 
 
-def data_calc():
-    operators = ['+', '-', '*']
-    number_1 = randint(0, 100)
-    number_2 = randint(0, 100)
-    number_3 = randint(0, 11)
-    random_operators = choice(['+', '-', '*'])
-    return number_1, number_2, number_3, operators, random_operators
+def data_calc(number_1, number_2, random_operator):
+    if random_operator == operators[0]:
+        result = number_1 + number_2
+    elif random_operator == operators[1]:
+        result = number_1 - number_2
+    elif random_operator == operators[2]:
+        result = number_1 * number_2
+    return result
 
 
 def get_question_and_right_answer():
-    number_1, number_2, number_3, operators, random_operators = data_calc()
-    if random_operators == operators[0]:
-        math_question = f'{number_1} + {number_2}'
-        result = number_1 + number_2
-    elif random_operators == operators[1]:
-        math_question = f'{number_1} - {number_2}'
-        result = number_1 - number_2
-    elif random_operators == operators[2]:
-        math_question = f'{number_1} * {number_3}'
-        result = number_1 * number_3
+    number_1 = randint(0, 20)
+    number_2 = randint(0, 20)
+    random_operator = choice(operators)
+    math_question = f'{number_1} {random_operator} {number_2}'
+    result = data_calc(number_1, number_2, random_operator)
     return math_question, result
