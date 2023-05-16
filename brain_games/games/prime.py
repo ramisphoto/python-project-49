9e4ad5ee27ca646d19220f96d77df10672ddd006
macbook <ramisphoto>
2023-05-16 15:34:07 +0300

    update README.md

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -9,10 +9,11 @@ def get_question_and_correct_answer():
     question = str(number)
     if number == 0 or 1:
         result = 'no'
-    div = 0
-    for i in range(2, number // 2 + 1):
-        if number % i == 0:
-            div = div + 1
-            break
-    result = 'yes' if div == 0 else 'no'
+    else:
+        div = 0
+        for i in range(2, number // 2 + 1):
+            if number % i == 0:
+                div = div + 1
+                break
+        result = 'yes' if div == 0 else 'no'
     return question, result
