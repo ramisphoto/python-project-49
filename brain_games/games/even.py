cebec671eac0ea93fd4744cd8edb71940668b59f
macbook <ramisphoto>
2023-05-29 14:25:15 +0300

    upgrate even, prime

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,8 +4,12 @@ from random import randint
 GAME_RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+def is_even(number):
+    return number % 2 == 0
+
+
 def get_question_and_correct_answer():
     number = randint(0, 1000)
     question = str(number)
-    result = 'yes' if number % 2 == 0 else 'no'
+    result = 'yes' if is_even(number) else 'no'
     return question, result
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,21 +4,19 @@ from random import randint
 GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def data_prime(number):
+def is_prime(number):
+    divisors = 0
     if number == 0 or number == 1:
-        result = 'no'
-    else:
-        div = 0
-        for i in range(2, number // 2 + 1):
-            if number % i == 0:
-                div = div + 1
-                break
-        result = 'yes' if div == 0 else 'no'
-    return result
+        divisors = 1
+    for i in range(2, number // 2 + 1):
+        if number % i == 0:
+            divisors += 1
+            break
+    return divisors == 0
 
 
 def get_question_and_correct_answer():
     number = randint(0, 100)
     question = str(number)
-    result = data_prime(number)
+    result = 'yes' if is_prime(number) else 'no'
     return question, result
